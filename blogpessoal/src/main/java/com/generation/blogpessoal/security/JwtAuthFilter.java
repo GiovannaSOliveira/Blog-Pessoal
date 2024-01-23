c98b582a7a0fd5c7b0c3c0ed3a429481c226f548
Giovanna Oliveira <giovannasoliveira58@gmail.com>
2024-01-23 11:43:44 -0300

    Finalização security

--- blogpessoal/src/main/java/com/generation/blogpessoal/security/JwtAuthFilter.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/security/JwtAuthFilter.java
@@ -14,56 +14,50 @@ import org.springframework.web.server.ResponseStatusException;
 
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.security.SignatureException;
 import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class JwtAuthFilter extends OncePerRequestFilter{
+public class JwtAuthFilter extends OncePerRequestFilter {
 
-	@Autowired
-	private JwtService jwtService;
-	
-	@Autowired
-	private UserDetailsServiceImpl userDetailsService;
+    @Autowired
+    private JwtService jwtService;
 
-	@Override
-	protected void doFilterInternal(HttpServletRequest request, 
-			HttpServletResponse response, FilterChain filterChain)
-			throws ServletException, IOException {
-		
-		String authHeader = request.getHeader("Authorization");
-		String token = null;
-		String username = null;
-	
-	
-	try {
-		if (authHeader != null && authHeader.startsWith("Bearer ")) {
-			token = authHeader.substring(7);
-			username = jwtService.extractUsername(username);
-		}
-		
-		if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-			UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-		
-		if(jwtService.validateToken(token, userDetails)) {
-			UsernamePasswordAuthenticationToken authToken =
-					new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-			
-			authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-			SecurityContextHolder.getContext().setAuthentication(authToken);
-		}
-		}
-		filterChain.doFilter(request, response);
-	}
-	catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException | SignatureException | ResponseStatusException e) {
-		response.setStatus(HttpStatus.FORBIDDEN.value());
-		return ;
-	}
-	
-	}
-	
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        String token = null;
+        String username = null;
+    
+        try{
+            if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                token = authHeader.substring(7);
+                username = jwtService.extractUsername(token);
+            }
+
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                    
+                if (jwtService.validateToken(token, userDetails)) {
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            
+            }
+            filterChain.doFilter(request, response);
+
+        }catch(ExpiredJwtException | UnsupportedJwtException | MalformedJwtException 
+                | SignatureException | ResponseStatusException e){
+            response.setStatus(HttpStatus.FORBIDDEN.value());
+            return;
+        }
+    }
 }
--- blogpessoal/src/main/java/com/generation/blogpessoal/security/JwtService.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/security/JwtService.java
@@ -18,50 +18,50 @@ import io.jsonwebtoken.security.Keys;
 @Component
 public class JwtService {
 
-	public static final String SECRET = "7df8721b9d52e146f8643b4ce5bd2a3d2714e6cb8b32d3c7f813cdbdf2acc4aa";
-	
+	public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+
 	private Key getSignKey() {
 		byte[] keyBytes = Decoders.BASE64.decode(SECRET);
 		return Keys.hmacShaKeyFor(keyBytes);
 	}
-	
-	private Claims exctractAllClaims(String token) {
+
+	private Claims extractAllClaims(String token) {
 		return Jwts.parserBuilder()
 				.setSigningKey(getSignKey()).build()
 				.parseClaimsJws(token).getBody();
 	}
-	
+
 	public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
-		final Claims claims = exctractAllClaims(token);
+		final Claims claims = extractAllClaims(token);
 		return claimsResolver.apply(claims);
 	}
-	
+
 	public String extractUsername(String token) {
 		return extractClaim(token, Claims::getSubject);
 	}
-	
+
 	public Date extractExpiration(String token) {
 		return extractClaim(token, Claims::getExpiration);
 	}
-	
-	public Boolean isTokenExpired(String token) {
+
+	private Boolean isTokenExpired(String token) {
 		return extractExpiration(token).before(new Date());
 	}
-	
+
 	public Boolean validateToken(String token, UserDetails userDetails) {
 		final String username = extractUsername(token);
 		return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
 	}
-	
+
 	private String createToken(Map<String, Object> claims, String userName) {
 		return Jwts.builder()
-				.setClaims(claims)
-				.setSubject(userName)
-				.setIssuedAt(new Date(System.currentTimeMillis()))
-				.setExpiration(new Date (System.currentTimeMillis() + 1000*60*60))
-				.signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
+					.setClaims(claims)
+					.setSubject(userName)
+					.setIssuedAt(new Date(System.currentTimeMillis()))
+					.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+					.signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
 	}
-	
+
 	public String generateToken(String userName) {
 		Map<String, Object> claims = new HashMap<>();
 		return createToken(claims, userName);
