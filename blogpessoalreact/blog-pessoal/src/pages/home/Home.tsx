f3c8518a173876bd2359773afd29d014a10b5cc1
Giovanna Oliveira <giovannasoliveira58@gmail.com>
2024-02-14 18:41:10 -0300

    Criação inicial react

---
+++ blogpessoalreact/blog-pessoal/src/pages/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className='titulo'>Home</h1>
+           
+           <img src="https://i.imgur.com/tJxoueU.jpeg" className='img' alt="placeholder" />
+        </>
+    );
+}
+
+export default Home;
